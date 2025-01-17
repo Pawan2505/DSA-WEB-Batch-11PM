a937736b5670077f70560dd983b3be3bbdc3dc0d
pawan2505 <pawanaktu@gmail.com>
2025-01-17 13:20:11 +0530

    Custom pattern

--- CPP Code/code.cpp
+++ CPP Code/code.cpp
@@ -3096,17 +3096,74 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//  int num = 1;
+//   for(int row = 1; row<=4; row++){
+//     for(int col = 1; col <= row; col++){
+//       cout<<num++;
+//     }
+//       cout<<endl;
+//   }
+
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//   for(int row = 1; row <=4; row++){
+//     //space
+
+//     for(int space = 1; space <= 4-row; space++){
+//       cout<<" ";
+//     }
+
+//     //triangle
+
+//     for(int triangle = 1; triangle<= 2*row-1; triangle++){
+//       if(row == 4){
+//         cout<<"*";
+//       }else{
+//         if(triangle == 1 || triangle == 2*row-1){
+//           cout<<"*";
+//         }else{
+//           cout<<" ";
+//         }
+//       }
+    
+//     }       
+
+//     cout<<endl;
+
+//   }
+// }
+
 #include<iostream>
 using namespace std;
 
 int main(){
 
- int num = 1;
-  for(int row = 1; row<=4; row++){
-    for(int col = 1; col <= row; col++){
-      cout<<num++;
-    }
-      cout<<endl;
-  }
+  for(int row = 1; row <=4; row++){
+    //space
 
+    for(int space = 1; space <= row-1; space++){
+      cout<<" ";
+    }
+
+    //triangle
+
+    for(int triangle = 7; triangle>= 2*row -1; triangle--){
+      cout<<"*";
+    }       
+
+    cout<<endl;
+
+  }
 }
