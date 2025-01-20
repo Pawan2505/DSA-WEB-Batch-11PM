ce0258e8a0085236e45c291c013156edb620b84b
pawan2505 <pawanaktu@gmail.com>
2025-01-20 12:27:34 +0530

    Array

--- CPP Code/code.cpp
+++ CPP Code/code.cpp
@@ -3145,25 +3145,151 @@
 //   }
 // }
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//   for(int row = 1; row <=4; row++){
+//     //space
+
+//     for(int space = 1; space <= row-1; space++){
+//       cout<<" ";
+//     }
+
+//     //triangle
+
+//     for(int triangle = 7; triangle>= 2*row -1; triangle--){
+//       cout<<"*";
+//     }       
+
+//     cout<<endl;
+
+//   }
+// }
+
+
+
+// https://medium.com/@yhmyown/data-types-in-c-a58d8eef1cb8
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//   int a = 10;
+
+//   wchar_t result = sizeof(a);  // this datatype is used to store size of variable or address or variable
+
+//   cout<<result;
+// }
+
+
+// Byte: A single letter is one byte.
+// Kilobyte (KB): One thousand bytes is one kilobyte.
+// Megabyte (MB): One thousand kilobytes is one megabyte.
+// Gigabyte (GB): One thousand megabytes is one gigabyte.
+
+
+// #include<iostream>
+// using namespace std;
+
+
+
+// int main(){
+//   int n = 10;
+
+//   int arr[n];
+
+//   arr[0] = 10;
+//   arr[1] = 20;
+//   arr[2] = 30;
+//   arr[3] = 40;
+//   arr[4] = 50;
+//   arr[5] = 60;
+//   arr[6] = 70;
+//   arr[7] = 80;
+//   arr[8] = 90;
+//   arr[9] = 100;
+
+//   cout<<arr[0]<<endl;
+//   cout<<&arr[0]<<endl;
+//   cout<<arr[1]<<endl;
+//   cout<<&arr[1]<<endl;
+//   cout<<arr[2]<<endl;
+//   cout<<&arr[2]<<endl;
+//   cout<<arr[3]<<endl;
+//   cout<<&arr[3]<<endl;
+//   cout<<arr[4]<<endl;
+//   cout<<&arr[4]<<endl;
+//   cout<<arr[5]<<endl;
+//   cout<<&arr[5]<<endl;
+//   cout<<arr[6]<<endl;
+//   cout<<&arr[6]<<endl;
+//   cout<<arr[7]<<endl;
+//   cout<<&arr[7]<<endl;
+//   cout<<arr[8]<<endl;
+//   cout<<&arr[8]<<endl;
+//   cout<<arr[9]<<endl;
+//   cout<<&arr[9]<<endl;
+
+
+
+
+
+// }
+
+// #include<iostream>
+// using namespace std;
+
+
+
+// int main(){
+//   int n = 10;
+
+//   int arr[n] ={10,20,30,40,50,60,70,80,90,100};
+
+
+
+//   cout<<arr[0]<<endl;
+//   cout<<&arr[0]<<endl;
+//   cout<<arr[1]<<endl;
+//   cout<<&arr[1]<<endl;
+//   cout<<arr[2]<<endl;
+//   cout<<&arr[2]<<endl;
+//   cout<<arr[3]<<endl;
+//   cout<<&arr[3]<<endl;
+//   cout<<arr[4]<<endl;
+//   cout<<&arr[4]<<endl;
+//   cout<<arr[5]<<endl;
+//   cout<<&arr[5]<<endl;
+//   cout<<arr[6]<<endl;
+//   cout<<&arr[6]<<endl;
+//   cout<<arr[7]<<endl;
+//   cout<<&arr[7]<<endl;
+//   cout<<arr[8]<<endl;
+//   cout<<&arr[8]<<endl;
+//   cout<<arr[9]<<endl;
+//   cout<<&arr[9]<<endl;
+
+
+// }
+
 #include<iostream>
 using namespace std;
 
+
+
 int main(){
+  int n = 10;
 
-  for(int row = 1; row <=4; row++){
-    //space
+  int arr[n] ={10,20,30,40,50,60,70,80,90,100};
 
-    for(int space = 1; space <= row-1; space++){
-      cout<<" ";
-    }
-
-    //triangle
-
-    for(int triangle = 7; triangle>= 2*row -1; triangle--){
-      cout<<"*";
-    }       
-
-    cout<<endl;
-
+  for(int i = 0; i < n; i++){
+    cout<<"Element present at "<<i<<" index : "<<arr[i]<<endl;
   }
+
+
 }
