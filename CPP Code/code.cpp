068a8c7514223972c430c71f4b749c4fb1e9d0e1
pawan2505 <pawanaktu@gmail.com>
2025-01-29 12:17:44 +0530

    recurssion function & Pointer

--- CPP Code/code.cpp
+++ CPP Code/code.cpp
@@ -4228,3 +4228,82 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+
+
+// int factorial(int num){
+
+//     // Base Condition
+
+//     if(num==1){
+//         return 1;
+//     }
+
+//     return num * factorial(num-1);
+// }
+
+// int main(){
+
+//     int num;
+//     cout<<"Enter Number : ";
+//     cin>>num;
+
+//     int result = factorial(num);
+
+//     cout<<num<<" Factorial : "<<result<<endl;
+    
+
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+// int addition(int num){
+
+//     // Base Condition
+
+//     if(num==1){
+//         return 1;
+//     }
+
+//     return num + addition(num-1);
+// }
+
+// int main(){
+
+//     int num;
+//     cout<<"Enter Number : ";
+//     cin>>num;
+
+//     int result = addition(num);
+
+//     cout<<" Addition : "<<result<<endl;
+    
+
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int a = 10;  // variable declared
+
+//     int *ptr = &a;  // pointer variable declared and assign address of a.
+
+// // value
+//     cout<<"a : "<<a<<endl;
+//     cout<<"*ptr : "<<*ptr<<endl;
+
+//     // address
+
+//     cout<<"Address of a : "<<&a<<endl;
+//     cout<<"Address of ptr : "<<ptr<<endl;
+
+// }
