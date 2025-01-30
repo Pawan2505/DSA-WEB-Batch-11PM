6b33f2872777a61ca28c55b98849203492e0adbc
pawan2505 <pawanaktu@gmail.com>
2025-01-30 12:28:02 +0530

    code

--- CPP Code/code.cpp
+++ CPP Code/code.cpp
@@ -4307,3 +4307,58 @@
 //     cout<<"Address of ptr : "<<ptr<<endl;
 
 // }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int a = 10;
+
+//     int *ptr1 = &a;
+
+//     int **ptr2 = &ptr1;
+
+//     cout<<*ptr1<<endl;  //10
+//     cout<<ptr1<<endl;
+
+//     cout<<&a<<endl;
+
+//     cout<<*ptr2<<endl;
+
+//     cout<<**ptr2<<endl;
+// }
+
+
+// Array pointer
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5] = {10,20,30,40,50};
+
+    // int *ptr = arr;  ->array ka base address -> &arr[0]
+
+    // cout<<arr<<endl;
+
+    // cout<<&arr[0]<<endl;
+
+
+    // int *ptr = arr;
+
+    // cout<<*ptr<<endl;  
+
+
+    int *ptr = arr;
+
+    for(int i = 0; i < 5; i++){
+        cout<<"Address of "<<i<<" index : "<<(ptr+i)<<" and its value : ";
+        cout<<*(ptr + i)<<endl;
+    }
+
+}
