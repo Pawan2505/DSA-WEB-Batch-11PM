b05f202a0159ef7ab629f98607bd983c4aa97f68
pawan2505 <pawanaktu@gmail.com>
2025-03-27 12:35:56 +0530

    searching

---
+++ revision/utkarsdought.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        int num = 1;
+        for (int j = 0; j <= i; j++)
+        {
+            
+            cout << num << " ";
+
+            if (j < i)
+            {
+                num = num * (i - j) / (j + 1);
+            }
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
